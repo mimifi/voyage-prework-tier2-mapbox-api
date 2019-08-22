70d1c34f073549298578c0d10f0784a8f5ecdf14
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-23 10:39:59 +1200

    Add Mapbox API to app and fix Readme file

--- src/app/mapbox/mapbox.component.ts
+++ src/app/mapbox/mapbox.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../../environments/environment";
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-mapbox',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapbox.component.css']
 })
 export class MapboxComponent implements OnInit {
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/dark-v10';
+  lat = 37.75;
+  lng = -122.41;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    (mapboxgl as typeof mapboxgl).accessToken = environment.mapbox.accessToken;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 9,
+      center: [this.lng, this.lat]
+    });
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+  }
 }
