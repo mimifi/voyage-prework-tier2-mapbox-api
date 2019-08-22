a4a1b8a01189b7e6225a88749e6fabc051f58ed9
Mantab Tadayon <mt.tadayon@gmail.com>
2019-08-22 15:07:27 +1200

    Add Mapbox to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapboxComponent } from './mapbox/mapbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapboxComponent
   ],
   imports: [
     BrowserModule
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWltaWZpIiwiYSI6ImNqem0xaml4eTBqZ2UzZXAxZWswdzJpaGUifQ.KIBwlkLEvDFr9j82zUuTeQ'
+  }
 };
 
 /*
